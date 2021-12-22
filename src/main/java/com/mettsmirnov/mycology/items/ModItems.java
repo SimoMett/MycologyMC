b344de268c6d1de74d04e71b053666162cfda871
matteo <matt.simonetti@hotmail.com>
2021-12-22 12:39:02 +0100

    Updated forge again (mappings wouldn't update by itself)

--- src/main/java/com/mettsmirnov/mycology/items/ModItems.java
+++ src/main/java/com/mettsmirnov/mycology/items/ModItems.java
@@ -1,13 +1,16 @@
 package com.mettsmirnov.mycology.items;
 
 import com.mettsmirnov.mycology.MycologyMod;
-import net.minecraft.item.Item;
+import net.minecraft.client.color.item.ItemColor;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MycologyMod.MODID);
 
+    //public static final RegistryObject
     //all the items below..
 }
