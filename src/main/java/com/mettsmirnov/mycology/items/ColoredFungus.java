055b8d735f1c62d01a5bc9263a86ffd7e1d92fe8
matteo <matt.simonetti@hotmail.com>
2021-12-22 12:39:13 +0100

    Starting ColoredFungus class

---
+++ src/main/java/com/mettsmirnov/mycology/items/ColoredFungus.java
@@ -0,0 +1,12 @@
+package com.mettsmirnov.mycology.items;
+
+import com.mettsmirnov.mycology.MycologyMod;
+import net.minecraft.world.item.Item;
+
+public class ColoredFungus extends Item
+{
+    public ColoredFungus()
+    {
+        super(new Properties().tab(MycologyMod.MOD_ITEM_GROUP));
+    }
+}
