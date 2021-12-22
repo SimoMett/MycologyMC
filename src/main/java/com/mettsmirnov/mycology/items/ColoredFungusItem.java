99b327c554c08fd39cb4e8b0881c228f7a5c7230
matteo <matt.simonetti@hotmail.com>
2021-12-22 18:55:31 +0100

    Got to display tooltip text only on the client

--- src/main/java/com/mettsmirnov/mycology/items/ColoredFungusItem.java
+++ src/main/java/com/mettsmirnov/mycology/items/ColoredFungusItem.java
@@ -11,6 +11,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -48,6 +50,7 @@ public class ColoredFungusItem extends Item implements ItemColor
     }
 
     @Override
+    @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack itemStack, @Nullable Level p_41422_, List<Component> tooltip, TooltipFlag p_41424_) {
         super.appendHoverText(itemStack, p_41422_, tooltip, p_41424_);
         tooltip.add(new TextComponent("\u00a77Some infos here pls"));
