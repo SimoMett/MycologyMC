91795d0cf0fc0e6c0c0fd9948c06c90c255dc69e
matteo <matt.simonetti@hotmail.com>
2021-12-31 15:43:09 +0100

    Renamed to ModCapabilitiesHandler

---
+++ src/main/java/com/mettsmirnov/mycology/capabilities/ModCapabilitiesHandler.java
@@ -0,0 +1,26 @@
+package com.mettsmirnov.mycology.capabilities;
+
+import com.mettsmirnov.mycology.MycologyMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ModCapabilitiesHandler
+{
+    public static final ResourceLocation GENOMA_CAPABILITY_RES = new ResourceLocation(MycologyMod.MODID,"fungus_genoma");
+
+    @SubscribeEvent
+    public void registerCapability(RegisterCapabilitiesEvent evt)
+    {
+        evt.register(IFungusData.class);
+    }
+
+    @SubscribeEvent
+    //TODO
+    public void attachCapability(AttachCapabilitiesEvent<ItemStack> evt)
+    {
+
+    }
+}
