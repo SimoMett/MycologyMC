2183c9febc85909882c1edef0f8dd6347a1bc168
matteo <matt.simonetti@hotmail.com>
2021-12-31 12:46:09 +0100

    Added CapabilityHandler.java

---
+++ src/main/java/com/mettsmirnov/mycology/capabilities/CapabilityHandler.java
@@ -0,0 +1,15 @@
+package com.mettsmirnov.mycology.capabilities;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class CapabilityHandler
+{
+    @SubscribeEvent
+    //TODO
+    public void onAttachCapability(AttachCapabilitiesEvent<ItemStack> evt)
+    {
+
+    }
+}
