57027de6c42f300bd3facfd552f0c2ff9599118f
matteo <matt.simonetti@hotmail.com>
2022-01-01 16:20:40 +0100

    Updating FungusData data model

--- src/main/java/com/mettsmirnov/mycology/capabilities/FungusData.java
+++ src/main/java/com/mettsmirnov/mycology/capabilities/FungusData.java
@@ -4,9 +4,109 @@ import net.minecraft.nbt.CompoundTag;
 
 public class FungusData implements IFungusData
 {
+
+
     @Override
     public int[] getColors() {
         return new int[4];
+    }
+
+    enum GeneType
+    {
+        DOMINANT,
+        RECESSIVE
+    }
+
+    //dominant genes
+    @Override
+    public String getDominantSpecies() {
+        return null;
+    }
+
+    @Override
+    public String getDominantSpreadingSpeed() {
+        return null;
+    }
+
+    @Override
+    public String getDominantRainBoostSpeed() {
+        return null;
+    }
+
+    @Override
+    public String getDominantLightRequirements() {
+        return null;
+    }
+
+    @Override
+    public String getDominantTerrainRequirement() {
+        return null;
+    }
+
+    @Override
+    public String getDominantHumidityRequirements() {
+        return null;
+    }
+
+    @Override
+    public String getDominantTemperatureRequirements() {
+        return null;
+    }
+
+    @Override
+    public String getDominantArea() {
+        return null;
+    }
+
+    @Override
+    public String getDominantEffect() {
+        return null;
+    }
+
+    //recessive genes
+    @Override
+    public String getRecessiveSpecies() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveSpreadingSpeed() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveRainBoostSpeed() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveLightRequirements() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveTerrainRequirement() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveHumidityRequirements() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveTemperatureRequirements() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveArea() {
+        return null;
+    }
+
+    @Override
+    public String getRecessiveEffect() {
+        return null;
     }
 
     @Override
--- src/main/java/com/mettsmirnov/mycology/capabilities/IFungusData.java
+++ src/main/java/com/mettsmirnov/mycology/capabilities/IFungusData.java
@@ -6,4 +6,26 @@ import net.minecraftforge.common.util.INBTSerializable;
 public interface IFungusData extends INBTSerializable<CompoundTag>
 {
     public int[] getColors();
+
+    //dominant genes
+    public String getDominantSpecies();
+    public String getDominantSpreadingSpeed();
+    public String getDominantRainBoostSpeed();
+    public String getDominantLightRequirements();
+    public String getDominantTerrainRequirement();
+    public String getDominantHumidityRequirements();
+    public String getDominantTemperatureRequirements();
+    public String getDominantArea();
+    public String getDominantEffect();
+
+    //recessive genes
+    public String getRecessiveSpecies();
+    public String getRecessiveSpreadingSpeed();
+    public String getRecessiveRainBoostSpeed();
+    public String getRecessiveLightRequirements();
+    public String getRecessiveTerrainRequirement();
+    public String getRecessiveHumidityRequirements();
+    public String getRecessiveTemperatureRequirements();
+    public String getRecessiveArea();
+    public String getRecessiveEffect();
 }
