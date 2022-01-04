9f0645074082fbd82a43b27a2becd15f683f0394
matteo <matt.simonetti@hotmail.com>
2022-01-04 20:11:19 +0100

    I've finally managed to render a fucking fungus block

--- src/main/java/com/mettsmirnov/mycology/MycologyMod.java
+++ src/main/java/com/mettsmirnov/mycology/MycologyMod.java
@@ -2,19 +2,22 @@ package com.mettsmirnov.mycology;
 
 import com.mettsmirnov.mycology.blocks.ModBlocks;
 import com.mettsmirnov.mycology.capabilities.FungusDataAttacher;
-import com.mettsmirnov.mycology.capabilities.FungusDataCapability;
 import com.mettsmirnov.mycology.capabilities.ModCapabilitiesHandler;
 import com.mettsmirnov.mycology.items.FungusItemColorer;
 import com.mettsmirnov.mycology.items.ModBlockItems;
 import com.mettsmirnov.mycology.items.ModItemGroup;
 import com.mettsmirnov.mycology.items.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.jline.utils.Log;
 
 @Mod(MycologyMod.MODID)
 public class MycologyMod
@@ -25,6 +28,9 @@ public class MycologyMod
     public MycologyMod()
     {
         IEventBus evtBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        evtBus.addListener(this::clientSetup);
+
         ModItems.ITEMS.register(evtBus);
         ModBlockItems.BLOCK_ITEMS.register(evtBus);
         ModBlocks.BLOCKS.register(evtBus);
@@ -35,6 +41,12 @@ public class MycologyMod
         MinecraftForge.EVENT_BUS.register(FungusDataAttacher.class);
     }
 
+    private void clientSetup(FMLClientSetupEvent event)
+    {
+        Log.info("TEST CLIENT SETUP");
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.COLORED_CRIMSON_FUNGUS.get(), RenderType.cutout());
+    }
     //private void enqueueIMC(final InterModEnqueueEvent event)
     //private void processIMC(final InterModProcessEvent event)
 }
--- src/main/java/com/mettsmirnov/mycology/blocks/ColoredFungusBlock.java
+++ src/main/java/com/mettsmirnov/mycology/blocks/ColoredFungusBlock.java
@@ -1,17 +1,30 @@
 package com.mettsmirnov.mycology.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ColoredFungusBlock extends BushBlock
 {
+    protected static final VoxelShape SHAPE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 9.0D, 12.0D);
+
     public ColoredFungusBlock()
     {
         super(BlockBehaviour.Properties.of(Material.PLANT)
                 .sound(SoundType.GRASS)
                 .noCollission()
                 .randomTicks());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE;
     }
 }
