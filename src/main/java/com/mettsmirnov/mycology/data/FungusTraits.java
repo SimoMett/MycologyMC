e3b4267ef643809ffac88034db6e8a8c05497651
matteo <matt.simonetti@hotmail.com>
2022-01-06 20:17:59 +0100

    Added FungusTraits class for a better construction of FungusData

---
+++ src/main/java/com/mettsmirnov/mycology/data/FungusTraits.java
@@ -0,0 +1,26 @@
+package com.mettsmirnov.mycology.data;
+
+public class FungusTraits
+{
+    public String species;
+    public Integer spreading;
+    public Float spreadboost;
+    public Integer light;
+    public String terrain;
+    public Integer humidity;
+    public Integer temp;
+    public Integer area;
+    public String effect;
+
+    public static final String[] traitsDictionary = new String[]{
+            "species",
+            "spreading",
+            "spreadboost",
+            "light",
+            "terrain",
+            "humidity",
+            "temp",
+            "area",
+            "effect"
+    };
+}
