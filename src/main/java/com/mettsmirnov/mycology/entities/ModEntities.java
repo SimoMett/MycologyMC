c1f09c75a4cf0db633ca6b8aed460779044bc5ed
matteo <matt.simonetti@hotmail.com>
2022-01-07 14:06:21 +0100

    Added the same entity to both crimson and warped fungus

--- src/main/java/com/mettsmirnov/mycology/entities/ModEntities.java
+++ src/main/java/com/mettsmirnov/mycology/entities/ModEntities.java
@@ -11,6 +11,5 @@ public class ModEntities
 {
     public static final DeferredRegister<BlockEntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MycologyMod.MODID);
 
-    public static final RegistryObject<BlockEntityType<ColoredFungusBlockEntity>> COLORED_CRIMSON_FUNGUS = ENTITIES.register("colored_crimson_fungus",()-> BlockEntityType.Builder.of(ColoredFungusBlockEntity::new, ModBlocks.COLORED_CRIMSON_FUNGUS.get()).build(null));
-    //public static final RegistryObject<BlockEntityType<ColoredFungusBlockEntity>> COLORED_WARPED_FUNGUS = ENTITIES.register("colored_warped_fungus",()-> BlockEntityType.Builder.of(ColoredFungusBlockEntity::new, ModBlocks.COLORED_WARPED_FUNGUS.get()).build(null));
+    public static final RegistryObject<BlockEntityType<ColoredFungusBlockEntity>> COLORED_CRIMSON_FUNGUS = ENTITIES.register("colored_crimson_fungus",()-> BlockEntityType.Builder.of(ColoredFungusBlockEntity::new, ModBlocks.COLORED_CRIMSON_FUNGUS.get(),ModBlocks.COLORED_WARPED_FUNGUS.get()).build(null));
 }
