72e761f01a1bee61b974cc47787c163d081c5f70
matteo <matt.simonetti@hotmail.com>
2022-05-08 14:44:23 +0200

    Removed log

--- src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipeLoader.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipeLoader.java
@@ -54,7 +54,6 @@ public class FungusBrewingRecipeLoader extends SimpleJsonResourceReloadListener
             if(poppedRecipe!=null)
             {
                 BrewingRecipeRegistry.addRecipe(poppedRecipe);
-                Log.info(poppedRecipe.species);
             }
         }
         while (poppedRecipe!=null);
