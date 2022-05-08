fe1a098f2040bd81c7e1b5a00e6be27c41f045d8
matteo <matt.simonetti@hotmail.com>
2022-05-08 11:16:43 +0200

    Fixed getOutput method. More work to do

--- src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipe.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipe.java
@@ -11,11 +11,13 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.PotionItem;
 import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionBrewing;
 import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraftforge.common.brewing.IBrewingRecipe;
 import net.minecraftforge.common.brewing.VanillaBrewingRecipe;
 
-public class FungusBrewingRecipe extends VanillaBrewingRecipe
+public class FungusBrewingRecipe implements IBrewingRecipe
 {
     String species;//FIXME should be private (testing only)
     private String resultPotion;
@@ -28,20 +30,36 @@ public class FungusBrewingRecipe extends VanillaBrewingRecipe
         potionLevel=level;
     }
 
-    @Override
+    public boolean isInput(ItemStack stack)
+    {
+        Item item = stack.getItem();
+        if(item == Items.POTION || item == Items.SPLASH_POTION || item == Items.LINGERING_POTION || item == Items.GLASS_BOTTLE)
+        {
+            Potion potion = PotionUtils.getPotion(stack);
+            return potion.delegate.name().getPath().equals("awkward");
+        }
+        return false;
+    }
+
     public boolean isIngredient(ItemStack ingredient)
     {
+        //TODO use species instead of any COLORED_CRIMSON_FUNGUS
         return ingredient.is(ModItems.COLORED_CRIMSON_FUNGUS.get());
     }
 
-    @Override
     public ItemStack getOutput(ItemStack input, ItemStack ingredient)
     {
-        //ItemStack superResult = super.getOutput(input, ingredient);
-        ItemStack result = new ItemStack(Items.POTION);
-        MobEffectInstance mobEffectInstance = new MobEffectInstance(MobEffects.POISON,potionLevel);
-        Potion potion = new Potion(resultPotion, mobEffectInstance);
-        PotionUtils.setPotion(result, potion);
-        return result;
+        //TODO add potionLevel to the result
+        if (!input.isEmpty() && !ingredient.isEmpty() && isIngredient(ingredient) && isInput(input))
+        {
+            ItemStack result = new ItemStack(Items.POTION);
+            PotionUtils.setPotion(result, Potion.byName(resultPotion));
+            if (result != input)
+            {
+                return result;
+            }
+            return ItemStack.EMPTY;
+        }
+        return ItemStack.EMPTY;
     }
 }
