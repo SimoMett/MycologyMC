a7f6602de5e263ea0b340c972a9bed7e6f3ad2b8
matteo <matt.simonetti@hotmail.com>
2022-05-08 15:55:24 +0200

    Started working on crafting_recipes branch

---
+++ src/main/java/com/mettsmirnov/mycology/recipes/crafting/FungusCraftingRecipe.java
@@ -0,0 +1,48 @@
+package com.mettsmirnov.mycology.recipes.crafting;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+
+public class FungusCraftingRecipe implements Recipe<Container>
+{
+
+    @Override
+    public boolean matches(Container p_44002_, Level p_44003_) {
+        return false;
+    }
+
+    @Override
+    public ItemStack assemble(Container p_44001_) {
+        return null;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int p_43999_, int p_44000_) {
+        return false;
+    }
+
+    @Override
+    public ItemStack getResultItem() {
+        return null;
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return null;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return null;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return null;
+    }
+}
---
+++ src/main/java/com/mettsmirnov/mycology/recipes/crafting/FungusCraftingRecipeSerializer.java
@@ -0,0 +1,8 @@
+package com.mettsmirnov.mycology.recipes.crafting;
+
+import net.minecraftforge.common.extensions.IForgeRecipeSerializer;
+
+public class FungusCraftingRecipeSerializer implements IForgeRecipeSerializer<FungusCraftingRecipe>
+{
+
+}
