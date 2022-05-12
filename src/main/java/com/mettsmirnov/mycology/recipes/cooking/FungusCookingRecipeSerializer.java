544338a2a01c19c4660279558ff9cfc559987c07
DonSimonetti <matt.simonetti@hotmail.com>
2022-05-12 14:32:55 +0200

    Changed type from SmeltingRecipe to FungusCookingRecipe

--- src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipeSerializer.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipeSerializer.java
@@ -6,30 +6,30 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SmeltingRecipe;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.ICondition;
 import net.minecraftforge.common.extensions.IForgeRecipeSerializer;
 import org.jetbrains.annotations.Nullable;
 import org.jline.utils.Log;
 
-public class FungusCookingRecipeSerializer implements RecipeSerializer<SmeltingRecipe>
+public class FungusCookingRecipeSerializer implements RecipeSerializer<FungusCookingRecipe>
 {
 
     @Override
-    public SmeltingRecipe fromJson(ResourceLocation p_44103_, JsonObject jsonObject)
+    public FungusCookingRecipe fromJson(ResourceLocation p_44103_, JsonObject jsonObject)
     {
-        Log.info("Json Borne");
         return null;
     }
 
     @Nullable
     @Override
-    public SmeltingRecipe fromNetwork(ResourceLocation p_44105_, FriendlyByteBuf p_44106_)
+    public FungusCookingRecipe fromNetwork(ResourceLocation p_44105_, FriendlyByteBuf p_44106_)
     {
         return null;
     }
 
     @Override
-    public void toNetwork(FriendlyByteBuf p_44101_, SmeltingRecipe p_44102_)
+    public void toNetwork(FriendlyByteBuf p_44101_, FungusCookingRecipe p_44102_)
     {
 
     }
@@ -37,7 +37,6 @@ public class FungusCookingRecipeSerializer implements RecipeSerializer<SmeltingR
     @Override
     public RecipeSerializer<?> setRegistryName(ResourceLocation name)
     {
-        //FIXME can't return null
         return null;
     }
 
