ffd1e3e3bfd7258e0f43b6c97dfff2f19839634b
DonSimonetti <matt.simonetti@hotmail.com>
2022-05-12 09:09:37 +0200

    Registered a recipe serializer

--- src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipeSerializer.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipeSerializer.java
@@ -1,15 +1,56 @@
 package com.mettsmirnov.mycology.recipes.cooking;
 
 import com.google.gson.JsonObject;
+import com.mettsmirnov.mycology.MycologyMod;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
 import net.minecraftforge.common.crafting.conditions.ICondition;
 import net.minecraftforge.common.extensions.IForgeRecipeSerializer;
+import org.jetbrains.annotations.Nullable;
+import org.jline.utils.Log;
 
-public class FungusCookingRecipeSerializer implements IForgeRecipeSerializer<FungusCookingRecipe>
+public class FungusCookingRecipeSerializer implements RecipeSerializer<SmeltingRecipe>
 {
+
     @Override
-    public FungusCookingRecipe fromJson(ResourceLocation recipeLoc, JsonObject recipeJson, ICondition.IContext context)
+    public SmeltingRecipe fromJson(ResourceLocation p_44103_, JsonObject jsonObject)
     {
-        return IForgeRecipeSerializer.super.fromJson(recipeLoc, recipeJson, context);
+        Log.info("Json Borne");
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public SmeltingRecipe fromNetwork(ResourceLocation p_44105_, FriendlyByteBuf p_44106_)
+    {
+        return null;
+    }
+
+    @Override
+    public void toNetwork(FriendlyByteBuf p_44101_, SmeltingRecipe p_44102_)
+    {
+
+    }
+
+    @Override
+    public RecipeSerializer<?> setRegistryName(ResourceLocation name)
+    {
+        //FIXME can't return null
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public ResourceLocation getRegistryName()
+    {
+        return new ResourceLocation(MycologyMod.MODID, "fungus_cooking");
+    }
+
+    @Override
+    public Class<RecipeSerializer<?>> getRegistryType()
+    {
+        return null;
     }
 }
