4c2d8a650cca9a76feee80737cea318d75f54846
DonSimonetti <matt.simonetti@hotmail.com>
2022-05-12 14:32:02 +0200

    Implementing FungusCookingRecipe

--- src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipe.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipe.java
@@ -1,12 +1,62 @@
 package com.mettsmirnov.mycology.recipes.cooking;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
 
-public class FungusCookingRecipe extends SmeltingRecipe {
+public class FungusCookingRecipe implements Recipe<Container>
+{
+    public String speciesIngredient;
+    public float experience;
+    public int cookingTime;
+    public ItemStack result;
 
-    public FungusCookingRecipe(ResourceLocation p_44460_, String p_44461_, Ingredient p_44462_, ItemStack p_44463_, float p_44464_, int p_44465_) {
-        super(p_44460_, p_44461_, p_44462_, p_44463_, p_44464_, p_44465_);
+    public FungusCookingRecipe(String speciesIngredient, float experience, int cookingTime, ItemStack result)
+    {
+        this.speciesIngredient = speciesIngredient;
+        this.experience = experience;
+        this.cookingTime = cookingTime;
+        this.result = result;
+    }
+
+    @Override
+    public boolean matches(Container p_44002_, Level p_44003_) {
+        return false;
+    }
+
+    @Override
+    public ItemStack assemble(Container p_44001_) {
+        return null;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int p_43999_, int p_44000_)
+    {
+        return false;
+    }
+
+    @Override
+    public ItemStack getResultItem() {
+        return null;
+    }
+
+    @Override
+    public ResourceLocation getId()
+    {
+        return null;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer()
+    {
+        return ModCookingRecipes.FUNGUS_COOKING_RECIPE_SERIALIZER.get();
+    }
+
+    @Override
+    public RecipeType<?> getType()
+    {
+        return RecipeType.SMELTING;
     }
 }
