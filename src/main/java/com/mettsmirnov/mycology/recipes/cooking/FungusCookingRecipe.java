ed01300247ed246ffb4083565cf7cd50f5e00f37
DonSimonetti <matt.simonetti@hotmail.com>
2022-05-12 20:01:04 +0200

    More stuff dont know

--- src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipe.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/cooking/FungusCookingRecipe.java
@@ -1,62 +1,23 @@
 package com.mettsmirnov.mycology.recipes.cooking;
 
+import com.mettsmirnov.mycology.items.ModItems;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.*;
-import net.minecraft.world.level.Level;
 
-public class FungusCookingRecipe implements Recipe<Container>
+public class FungusCookingRecipe extends AbstractCookingRecipe
 {
     public String speciesIngredient;
-    public float experience;
-    public int cookingTime;
-    public ItemStack result;
 
-    public FungusCookingRecipe(String speciesIngredient, float experience, int cookingTime, ItemStack result)
+    public FungusCookingRecipe(ResourceLocation id, String speciesIngredient, ItemStack result, float exp, int cookingTime)
     {
+        super(RecipeType.SMELTING, id, "funguscooking", Ingredient.of(ModItems.COLORED_CRIMSON_FUNGUS.get()), result, exp, cookingTime);
         this.speciesIngredient = speciesIngredient;
-        this.experience = experience;
-        this.cookingTime = cookingTime;
-        this.result = result;
-    }
-
-    @Override
-    public boolean matches(Container p_44002_, Level p_44003_) {
-        return false;
-    }
-
-    @Override
-    public ItemStack assemble(Container p_44001_) {
-        return null;
-    }
-
-    @Override
-    public boolean canCraftInDimensions(int p_43999_, int p_44000_)
-    {
-        return false;
-    }
-
-    @Override
-    public ItemStack getResultItem() {
-        return null;
-    }
-
-    @Override
-    public ResourceLocation getId()
-    {
-        return null;
     }
 
     @Override
     public RecipeSerializer<?> getSerializer()
     {
         return ModCookingRecipes.FUNGUS_COOKING_RECIPE_SERIALIZER.get();
-    }
-
-    @Override
-    public RecipeType<?> getType()
-    {
-        return RecipeType.SMELTING;
     }
 }
