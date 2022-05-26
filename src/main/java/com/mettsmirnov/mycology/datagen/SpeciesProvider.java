7e936898ea28acaf1250a6b1d402a685ed178f41
DonSimonetti <matt.simonetti@hotmail.com>
2022-05-26 17:18:37 +0200

    Prototyping the createSpecies function

--- src/main/java/com/mettsmirnov/mycology/datagen/SpeciesProvider.java
+++ src/main/java/com/mettsmirnov/mycology/datagen/SpeciesProvider.java
@@ -5,6 +5,8 @@ import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
 import org.jline.utils.Log;
 
+import java.awt.*;
+
 public class SpeciesProvider implements DataProvider
 {
     private DataGenerator generator;
@@ -16,7 +18,36 @@ public class SpeciesProvider implements DataProvider
     @Override
     public void run(HashCache p_123925_)
     {
-        Log.info("reached");
+        createSpecies("Red amanita",
+                "colored_crimson_fungus",
+                new Color[]{new Color(16777164),new Color(16724736), new Color(15921906), new Color(15921906)},
+                25,
+                1.0f,
+                15,
+                "grass",
+                0.8f,
+                0.25f,
+                3,
+                "none",
+                new FungusSpawn("",0.5f)
+        );
+    }
+
+    private static class FungusSpawn
+    {
+        public String biomes;
+        public float chance;
+
+        public FungusSpawn(String biomes, float chance)
+        {
+            this.biomes = biomes;
+            this.chance = chance;
+        }
+    }
+
+    private static void createSpecies(String name, String type, Color[] colors, float spreading, float spreadBoost, int light, String terrain, float humidity, float temperature, int area, String effect, FungusSpawn spawnType)
+    {
+
     }
 
     @Override
