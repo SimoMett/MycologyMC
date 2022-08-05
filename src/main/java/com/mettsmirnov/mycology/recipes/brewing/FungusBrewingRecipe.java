c0764146f383d4b8c748433026e9908972eff71b
matteo <matt.simonetti@hotmail.com>
2022-08-05 12:22:29 +0200

    Method 'isInput' is working again

--- src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipe.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipe.java
@@ -3,23 +3,13 @@ package com.mettsmirnov.mycology.recipes.brewing;
 import com.mettsmirnov.mycology.capabilities.FungusDataCapability;
 import com.mettsmirnov.mycology.capabilities.IFungusData;
 import com.mettsmirnov.mycology.items.ModItems;
-import com.mojang.blaze3d.shaders.Effect;
-import net.minecraft.client.renderer.EffectInstance;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.PotionItem;
 import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.PotionBrewing;
 import net.minecraft.world.item.alchemy.PotionUtils;
-import net.minecraft.world.item.alchemy.Potions;
 import net.minecraftforge.common.brewing.IBrewingRecipe;
-import net.minecraftforge.common.brewing.VanillaBrewingRecipe;
-
-import java.util.Objects;
 
 public class FungusBrewingRecipe implements IBrewingRecipe
 {
@@ -37,9 +27,8 @@ public class FungusBrewingRecipe implements IBrewingRecipe
         Item item = stack.getItem();
         if(item == Items.POTION || item == Items.SPLASH_POTION || item == Items.LINGERING_POTION || item == Items.GLASS_BOTTLE)
         {
-            Potion potion = PotionUtils.getPotion(stack);
-            //return potion.delegate.name().getPath().equals("awkward"); //FIXME
-            return true;
+            ResourceLocation resourceLocation = new ResourceLocation(stack.getTag().getString("Potion"));
+            return resourceLocation.getPath().equals("awkward");
         }
         return false;
     }
