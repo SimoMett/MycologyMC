e71b959f743943adc3bea6748cb72874c7cbeacb
DonSimonetti <matt.simonetti@hotmail.com>
2022-08-10 10:54:13 +0200

    New registry object

--- src/main/java/com/mettsmirnov/mycology/world/features/ModFeatures.java
+++ src/main/java/com/mettsmirnov/mycology/world/features/ModFeatures.java
@@ -2,10 +2,14 @@ package com.mettsmirnov.mycology.world.features;
 
 import com.mettsmirnov.mycology.MycologyMod;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModFeatures
 {
     public static final DeferredRegister<Feature<?>> MOD_FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, MycologyMod.MODID);
+
+    public static final RegistryObject<Feature<SimpleBlockConfiguration>> MUSHROOMS = MOD_FEATURES.register("mycology_mushrooms",RandomFungusFeature::new);
 }
