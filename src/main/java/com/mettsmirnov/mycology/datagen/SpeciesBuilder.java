33277526dd78cf016769dee14f3f81351b09dd48
DonSimonetti <matt.simonetti@hotmail.com>
2022-09-20 23:47:18 +0200

    Added SpeciesBuilder (experimental)

---
+++ src/main/java/com/mettsmirnov/mycology/datagen/SpeciesBuilder.java
@@ -0,0 +1,127 @@
+package com.mettsmirnov.mycology.datagen;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.mettsmirnov.mycology.MycologyMod;
+import net.minecraft.data.CachedOutput;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
+import net.minecraft.server.packs.PackType;
+import org.jline.utils.Log;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class SpeciesBuilder
+{
+    //Traits
+    private String speciesName;
+    private String type;
+    private int[] colors;
+    private int spreading;
+    private float spreadBoost;
+    private int light;
+    private String terrain;
+    private SpeciesProvider.BiomesSpecs biomesSpecs;
+    private SpeciesProvider.AreaEffect areaEffect;
+    private SpeciesProvider.FungusSpawn spawnType;
+
+    //Methods chaining
+    public SpeciesBuilder(String speciesName)
+    {
+        this.speciesName = speciesName;
+    }
+
+    public SpeciesBuilder type(String type)
+    {
+        this.type = type;
+        return this;
+    }
+
+    public SpeciesBuilder colors(int[] colors)
+    {
+        this.colors = colors;
+        return this;
+    }
+
+    public SpeciesBuilder spreading(int spreading)
+    {
+        this.spreading = spreading;
+        return this;
+    }
+
+    public SpeciesBuilder spreadBoost(float spreadBoost)
+    {
+        this.spreadBoost = spreadBoost;
+        return this;
+    }
+
+    public SpeciesBuilder light(int light)
+    {
+        this.light = light;
+        return this;
+    }
+
+    public SpeciesBuilder terrain(String terrain)
+    {
+        this.terrain = terrain;
+        return this;
+    }
+
+    public SpeciesBuilder biomesSpecs(SpeciesProvider.BiomesSpecs biomesSpecs)
+    {
+        this.biomesSpecs = biomesSpecs;
+        return this;
+    }
+
+    public SpeciesBuilder areaEffect(SpeciesProvider.AreaEffect areaEffect)
+    {
+        this.areaEffect = areaEffect;
+        return this;
+    }
+
+    public SpeciesBuilder spawnType(SpeciesProvider.FungusSpawn spawnType)
+    {
+        this.spawnType = spawnType;
+        return this;
+    }
+
+    public void build(DataGenerator generator, CachedOutput hashCache)
+    {
+        //IMPORTANT TODO: sanitize speciesName string
+        JsonObject fungusJson = new JsonObject();
+        fungusJson.addProperty("species",speciesName);
+        fungusJson.addProperty("type",type);
+        JsonArray jsonColors = new JsonArray();
+        for(int i = 0; i<4; i++)
+            jsonColors.add(colors[i]);
+        fungusJson.add("colors", jsonColors);
+        fungusJson.addProperty("spreading",spreading);
+        fungusJson.addProperty("spreadboost",spreadBoost);
+        fungusJson.addProperty("light",light);
+        fungusJson.addProperty("terrain",terrain);
+        fungusJson.addProperty("humidity",biomesSpecs.humidity);
+        fungusJson.addProperty("temperature",biomesSpecs.temperature);
+        fungusJson.addProperty("area",areaEffect.areaRadius);
+        fungusJson.addProperty("effect",areaEffect.effect);
+        if(spawnType!=null)
+        {
+            JsonObject spawnJson = new JsonObject();
+            spawnJson.addProperty("biomes", spawnType.biomes);
+            spawnJson.addProperty("chance",spawnType.chance);
+            fungusJson.add("spawn", spawnJson);
+        }
+
+        Path path = generator.getOutputFolder();
+        String jsonFileName = speciesName.toLowerCase().replace(' ', '_')+".json";
+        Path jsonLocation = path.resolve(String.join("/", PackType.SERVER_DATA.getDirectory(), MycologyMod.MODID, "fungi", jsonFileName));
+        try
+        {
+            DataProvider.saveStable(hashCache,fungusJson,jsonLocation);
+        }
+        catch (IOException e)
+        {
+            Log.error(e);
+        }
+    }
+
+}
