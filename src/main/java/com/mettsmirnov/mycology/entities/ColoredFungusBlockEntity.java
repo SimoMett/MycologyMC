b2f0dad54fa6fc893c2ecc048b4f754e0d85cd72
matteo <matt.simonetti@hotmail.com>
2023-05-03 13:25:55 +0200

    Further work into implementing area effects

--- src/main/java/com/mettsmirnov/mycology/entities/ColoredFungusBlockEntity.java
+++ src/main/java/com/mettsmirnov/mycology/entities/ColoredFungusBlockEntity.java
@@ -1,6 +1,7 @@
 package com.mettsmirnov.mycology.entities;
 
-import com.mettsmirnov.mycology.capabilities.FungusData;
+import com.mettsmirnov.mycology.capabilities.FungusDataCapability;
+import com.mettsmirnov.mycology.capabilities.FungusDataModel;
 import com.mettsmirnov.mycology.capabilities.IFungusData;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
@@ -8,8 +9,9 @@ import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,11 +19,13 @@ import net.minecraft.world.phys.AABB;
 import org.jetbrains.annotations.Nullable;
 import org.jline.utils.Log;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 public class ColoredFungusBlockEntity extends BlockEntity
 {
-    private final IFungusData fungusData = new FungusData();
+    private final IFungusData fungusDataModel = new FungusDataModel();
 
     public ColoredFungusBlockEntity(BlockPos blockPos, BlockState blockState)
     {
@@ -30,16 +34,68 @@ public class ColoredFungusBlockEntity extends BlockEntity
 
     //READ PLEASE
     /*
-    * According to my understanding, there's no need to override the load() and save() methods
-    * because the capability seems to already load and save data itself.
-    * */
-    /*@Override
+    * According to my understanding, there's no need to override the load and save method
+    * because the capability seems to already loading and saving data itself.
+    *
+    @Override
     public void load(CompoundTag tag)
     {
         CompoundTag capTag = tag.getCompound("ForgeCaps").getCompound(MycologyMod.MODID+":fungus_data");
-        //fungusData.deserializeNBT(capTag);
+        fungusData.deserializeNBT(capTag);
         super.load(tag);
     }*/
+
+    private static Instant lastInstant = Instant.now();
+    public void tick()
+    {
+        Level world = this.getLevel();
+        BlockPos pos = this.getBlockPos();
+        IFungusData fungusData = world.getBlockEntity(pos).getCapability(FungusDataCapability.INSTANCE).resolve().get();
+
+        // FIXME check when this occurs
+        if(fungusData.getField(FungusDataModel.AREA, IFungusData.GeneType.DOMINANT)=="none")
+            return;
+        //
+
+        int areaRadius = (Integer) fungusData.getField(FungusDataModel.AREA, IFungusData.GeneType.DOMINANT);
+        List<LivingEntity> entityList = getEntityListInAreaRadius(areaRadius);
+        String fungusEffect = (String) fungusData.getField(FungusDataModel.EFFECT, IFungusData.GeneType.DOMINANT);
+
+        for(LivingEntity entity : entityList)
+        {
+            if(entity.distanceToSqr(pos.getCenter()) < areaRadius)
+            {
+                if(Duration.between(lastInstant,Instant.now()).getSeconds() > 1f)
+                {
+                    applyEffectToEntity(entity,fungusEffect);
+                    lastInstant = Instant.now();
+                }
+            }
+        }
+    }
+
+    private void applyEffectToEntity(LivingEntity entity, String effectString)
+    {
+        //TODO convert effectString to actual MobEffectInstance
+        final int numOfTicks = 100; //this value is hardcoded and should not be configurable.
+        entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST,numOfTicks));
+    }
+
+    private List<LivingEntity> getEntityListInAreaRadius(int radius)
+    {
+        Level level = this.getLevel();
+        BlockPos pos = this.getBlockPos();
+        AABB boxArea = new AABB(
+                pos.getX() - (radius+1),
+                pos.getY() - (radius+1),
+                pos.getZ() - (radius+1),
+                pos.getX() + radius,
+                pos.getY() + radius,
+                pos.getZ() + radius
+        );
+        assert level != null;
+        return level.getEntitiesOfClass(LivingEntity.class, boxArea);
+    }
 
     //Server-Client synchronization
     @Override
@@ -66,30 +122,5 @@ public class ColoredFungusBlockEntity extends BlockEntity
     public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt)
     {
         super.onDataPacket(net, pkt);
-    }
-
-    public void tick()
-    {
-        /*TODO
-        * find first all entities inside a boxArea that is bigger then testRadius
-        * then for each entity check if distance is less then testRadius
-        * */
-        float testRadius = 3f;
-        Level level = this.getLevel();
-        BlockPos pos = this.getBlockPos();
-        BlockState blockState = this.getBlockState();
-        AABB boxArea = new AABB(
-                pos.getX() - (testRadius+1),
-                pos.getY() - (testRadius+1),
-                pos.getZ() - (testRadius+1),
-                pos.getX() + testRadius,
-                pos.getY() + testRadius,
-                pos.getZ() + testRadius
-        );
-        List<LivingEntity> entityList = level.getEntitiesOfClass(LivingEntity.class, boxArea);
-        for(LivingEntity entity : entityList)
-        {
-            Log.info(entity.distanceToSqr(pos.getCenter()));
-        }
     }
 }
