3994dd12bcd239bae28274a75dd00776f00cc71d
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-11 09:20:39 +0200

    Pushed FungusGeni

---
+++ src/main/java/com/mettsmirnov/mycology/datagen/common/FungusGeni.java
@@ -0,0 +1,18 @@
+package com.mettsmirnov.mycology.datagen.common;
+
+public class FungusGeni
+{
+    public static String AGARICUS = "Agaricus"; //(Champignon, prataioli)
+    public static String AMANITA = "Amanita"; //(velenoso)
+    public static String BOLETELLUS = "Boletellus"; // (
+    public static String BOLETUS = "Boletus"; //(porcini, commestibile o velenoso)
+    public static String CHALCIPORUS = "Chalciporus"; //(porcino pepato, commestibile)
+    public static String GALERINA = "Galerina"; //(galerina marginata, velenoso)
+    public static String LACCARIA = "Laccaria"; //(commestibile)
+    public static String LACTARIUS = "Lactarius"; //(pennenciole, commestibili o velenosi)
+    public static String LECCINUM = "Leccinum";//(leccino, commestibili)
+    public static String RUSSULA = "Russula";//(rossola, commestibile o neutro)
+    public static String SUILLUS = "Suillus"; //(Pinarelli, commestibili)
+    public static String TRICHOLOMOPSIS= "Tricholomopsis"; //(si potrebbe usare tipo Tricholomopsis evanescens per indicare un "ender fungus") (commestibile, neutro o tossico)
+    public static String XEROCOMUS = "Xerocomus"; //(commestibile)
+}
