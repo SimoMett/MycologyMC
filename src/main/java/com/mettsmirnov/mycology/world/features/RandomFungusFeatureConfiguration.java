692604e250b22c6692bbd3b7b6094f604d1ae7f4
matteo <matt.simonetti@hotmail.it>
2024-01-20 22:04:03 +0100

    Fixed crash

--- src/main/java/com/mettsmirnov/mycology/world/features/RandomFungusFeatureConfiguration.java
+++ src/main/java/com/mettsmirnov/mycology/world/features/RandomFungusFeatureConfiguration.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import org.jline.utils.Log;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomFungusFeatureConfiguration extends Feature<SimpleBlockConfiguration>
@@ -24,38 +25,40 @@ public class RandomFungusFeatureConfiguration extends Feature<SimpleBlockConfigu
     @Override
     public boolean place(FeaturePlaceContext<SimpleBlockConfiguration> placeContext)
     {
-        BlockPos origin = placeContext.origin();
-        String biomeName = placeContext.level().registryAccess().registryOrThrow(Registries.BIOME).getKey(placeContext.level().getBiome(origin).get()).toString();
-        float biomeTemp = placeContext.level().getBiome(origin).get().getModifiedClimateSettings().temperature();
-        float biomeDownfall = placeContext.level().getBiome(origin).get().getModifiedClimateSettings().downfall();
-        Log.info(biomeName, ": ", biomeTemp, ", ", biomeDownfall);
-
-        //get random species
-        Random random = new Random();
-        FungusSpeciesHandler.FungusSpecies randomSpecies = FungusSpeciesHandler.INSTANCE.getSpeciesList()
-                .get(random.nextInt(FungusSpeciesHandler.INSTANCE.getSpeciesList().size()));
-
-        //spawn fungus with the correct type
-        if(randomSpecies.fungusType.equals("colored_crimson_fungus")) //FIXME I don't like this plain string, it's better to use a const
-            placeContext.level().setBlock(origin, ModBlocks.COLORED_CRIMSON_FUNGUS.get().defaultBlockState(), 0); //WTF is the third parameter??
-        else
-            placeContext.level().setBlock(origin, ModBlocks.COLORED_WARPED_FUNGUS.get().defaultBlockState(), 0);
-
-        //edit its block entity
-        BlockEntity blockEntity = placeContext.level().getBlockEntity(origin);
-        if(blockEntity instanceof ColoredFungusBlockEntity)
+        ArrayList<FungusSpeciesHandler.FungusSpecies> speciesList = FungusSpeciesHandler.INSTANCE.getSpeciesList();
+        if(!speciesList.isEmpty())
         {
-            ColoredFungusBlockEntity coloredFungusBlockEntity = (ColoredFungusBlockEntity) blockEntity;
+            BlockPos origin = placeContext.origin();
+            String biomeName = placeContext.level().registryAccess().registryOrThrow(Registries.BIOME).getKey(placeContext.level().getBiome(origin).get()).toString();
+            float biomeTemp = placeContext.level().getBiome(origin).get().getModifiedClimateSettings().temperature();
+            float biomeDownfall = placeContext.level().getBiome(origin).get().getModifiedClimateSettings().downfall();
+            Log.info(biomeName, ": ", biomeTemp, ", ", biomeDownfall);
 
-            //FIXME these two line below are no good. I want to call one method only
-            coloredFungusBlockEntity.getFungusData().setColors(randomSpecies.colors);
-            coloredFungusBlockEntity.getFungusData().loadFrom(randomSpecies.defaultTraits, randomSpecies.defaultTraits);
+            //get random species
+            Random random = new Random();
+            FungusSpeciesHandler.FungusSpecies randomSpecies = speciesList
+                    .get(random.nextInt(FungusSpeciesHandler.INSTANCE.getSpeciesList().size()));
+
+            //spawn fungus with the correct type
+            if (randomSpecies.fungusType.equals("colored_crimson_fungus")) //FIXME I don't like this plain string, it's better to use a const
+                placeContext.level().setBlock(origin, ModBlocks.COLORED_CRIMSON_FUNGUS.get().defaultBlockState(), 0); //WTF is the third parameter??
+            else
+                placeContext.level().setBlock(origin, ModBlocks.COLORED_WARPED_FUNGUS.get().defaultBlockState(), 0);
+
+            //edit its block entity
+            BlockEntity blockEntity = placeContext.level().getBlockEntity(origin);
+            if (blockEntity instanceof ColoredFungusBlockEntity coloredFungusBlockEntity)
+            {
+                //FIXME these two line below are no good. I want to call one method only
+                coloredFungusBlockEntity.getFungusData().setColors(randomSpecies.colors);
+                coloredFungusBlockEntity.getFungusData().loadFrom(randomSpecies.defaultTraits, randomSpecies.defaultTraits);
+            } else {
+                Log.error("WHY THIS BLOCKENTITY ISN'T A COLORED FUNGUS?");
+                return false;
+            }
+            return true;
         }
         else
-        {
-            Log.error("WHY THIS BLOCKENTITY ISN'T A COLORED FUNGUS?");
             return false;
-        }
-        return true;
     }
 }
