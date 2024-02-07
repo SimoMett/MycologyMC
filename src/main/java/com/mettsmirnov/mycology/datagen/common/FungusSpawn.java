7174db2e8784c3d14f84b32beb752998580140b3
matteo <matt.simonetti@hotmail.it>
2024-02-07 11:54:27 +0100

    Updated FungusSpawn class

--- src/main/java/com/mettsmirnov/mycology/datagen/common/FungusSpawn.java
+++ src/main/java/com/mettsmirnov/mycology/datagen/common/FungusSpawn.java
@@ -3,7 +3,8 @@ package com.mettsmirnov.mycology.datagen.common;
 public class FungusSpawn
 {
     public static final FungusSpawn DEFAULT = new FungusSpawn("",.5f);
-    public static final FungusSpawn DARK_FOREST = new FungusSpawn("dark_forest", .5f);
+    public static final FungusSpawn DARK_FOREST = new FungusSpawn("minecraft:dark_forest", .5f);
+    public static final FungusSpawn SWAMP = new FungusSpawn("minecraft:swamp", .5f);
 
     public String biomes;
     public float chance;
