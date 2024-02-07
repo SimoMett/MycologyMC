08f208ecab9c5c7785098b10f1f7d131e3a14567
matteo <matt.simonetti@hotmail.it>
2024-02-07 16:44:16 +0100

    Added NO_SPAWN spawn info

--- src/main/java/com/mettsmirnov/mycology/datagen/SpeciesBuilder.java
+++ src/main/java/com/mettsmirnov/mycology/datagen/SpeciesBuilder.java
@@ -60,7 +60,7 @@ public class SpeciesBuilder
         this.terrain = "mycologymod:grass";
         this.biomesSpecs = BiomesSpecs.FOREST;
         this.areaEffect = FungusEffects.NO_EFFECT;
-        this.spawnType = null;
+        this.spawnType = FungusSpawn.NO_SPAWN;
         return this;
     }
 
