bcf9877ef9ba06126022e5d965c38a6f4db8f454
matteo <matt.simonetti@hotmail.it>
2024-02-07 11:32:38 +0100

    Extracted strings

--- src/main/java/com/mettsmirnov/mycology/blocks/ModBlocks.java
+++ src/main/java/com/mettsmirnov/mycology/blocks/ModBlocks.java
@@ -2,8 +2,6 @@ package com.mettsmirnov.mycology.blocks;
 
 import com.mettsmirnov.mycology.MycologyMod;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BushBlock;
-import net.minecraft.world.level.block.MushroomBlock;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,6 +10,8 @@ public class ModBlocks
 {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MycologyMod.MODID);
 
-    public static final RegistryObject<Block> COLORED_CRIMSON_FUNGUS = BLOCKS.register("colored_crimson_fungus",ColoredFungusBlock::new);
-    public static final RegistryObject<Block> COLORED_WARPED_FUNGUS = BLOCKS.register("colored_warped_fungus",ColoredFungusBlock::new);
+    public static final String COLORED_CRIMSON_STRING = "colored_crimson_fungus";
+    public static final String COLORED_WARPED_STRING = "colored_warped_fungus";
+    public static final RegistryObject<Block> COLORED_CRIMSON_FUNGUS = BLOCKS.register(COLORED_CRIMSON_STRING,ColoredFungusBlock::new);
+    public static final RegistryObject<Block> COLORED_WARPED_FUNGUS = BLOCKS.register(COLORED_WARPED_STRING,ColoredFungusBlock::new);
 }
