2501e69ebef074f42299a240168d40a8fe16f774
matteo <matt.simonetti@hotmail.it>
2024-02-13 18:17:13 +0100

    Added new constructor

--- src/main/java/com/mettsmirnov/mycology/datagen/common/FungusSpawn.java
+++ src/main/java/com/mettsmirnov/mycology/datagen/common/FungusSpawn.java
@@ -1,11 +1,16 @@
 package com.mettsmirnov.mycology.datagen.common;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+
 public class FungusSpawn
 {
     public static final FungusSpawn NO_SPAWN = null;
     public static final FungusSpawn ANY_BIOME = new FungusSpawn("",.5f);
     public static final FungusSpawn PLAINS = new FungusSpawn("minecraft:plains", .5f);
     public static final FungusSpawn FOREST = new FungusSpawn("minecraft:forest", .5f);
+    public static final FungusSpawn FLOWER_FOREST = new FungusSpawn(Biomes.FLOWER_FOREST, .5f);
     public static final FungusSpawn DARK_FOREST = new FungusSpawn("minecraft:dark_forest", .5f);
     public static final FungusSpawn BIRCH_FOREST = new FungusSpawn("minecraft:birch_forest", .5f);
     public static final FungusSpawn OLD_GROWTH_BIRCH_FOREST = new FungusSpawn("minecraft:old_growth_birch_forest", .5f);
@@ -22,6 +27,12 @@ public class FungusSpawn
     public FungusSpawn(String biomes, float chance)
     {
         this.biomes = biomes;
+        this.chance = chance;
+    }
+
+    public FungusSpawn(ResourceKey<Biome> biomeResourceKey, float chance)
+    {
+        this.biomes = biomeResourceKey.location().toString();
         this.chance = chance;
     }
 }
