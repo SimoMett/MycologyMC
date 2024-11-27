3b3065a8ecad825f31877d613166144ac2bc23aa
matteo <matt.simonetti@hotmail.it>
2024-11-27 16:10:05 +0100

    Implemented TeleportingEffect

--- src/main/java/com/mettsmirnov/mycology/effects/PlayerEffects/TeleportingEffect.java
+++ src/main/java/com/mettsmirnov/mycology/effects/PlayerEffects/TeleportingEffect.java
@@ -2,6 +2,7 @@ package com.mettsmirnov.mycology.effects.PlayerEffects;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.player.Player;
 
 public class TeleportingEffect extends MobEffect
 {
@@ -9,5 +10,10 @@ public class TeleportingEffect extends MobEffect
 
     protected TeleportingEffect() {
         super(MobEffectCategory.NEUTRAL, 0);
+    }
+
+    public static boolean shouldTeleport(Player player)
+    {
+        return player.getMainHandItem().isEmpty() && player.hasEffect(ModEffects.TELEPORTING.get());
     }
 }
