768b4da42eff2e8b6e20271b72bf7f49a53f0061
matteo <matt.simonetti@hotmail.it>
2024-12-03 22:39:51 +0100

    Fixed FungusBrewingRecipe accepting only CRIMSON type

--- src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipe.java
+++ src/main/java/com/mettsmirnov/mycology/recipes/brewing/FungusBrewingRecipe.java
@@ -35,7 +35,7 @@ public class FungusBrewingRecipe implements IBrewingRecipe
 
     public boolean isIngredient(ItemStack ingredient)
     {
-        if (ingredient.is(ModItems.COLORED_CRIMSON_FUNGUS.get()))
+        if (ingredient.is(ModItems.COLORED_CRIMSON_FUNGUS.get()) || ingredient.is(ModItems.COLORED_WARPED_FUNGUS.get()))
         {
             String species = (String)ingredient.getCapability(FungusDataCapability.INSTANCE).resolve().get().getField("species", IFungusData.GeneType.DOMINANT);
             return species.equals(this.species);
