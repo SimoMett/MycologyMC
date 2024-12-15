edc5562c65d18d1a60299b06e11858f25e35d809
matteo <matt.simonetti@hotmail.it>
2024-12-15 23:02:38 +0100

    Fixed 'isInput' method

--- src/main/java/com/simomett/mycologymod/recipes/brewing/FungusBrewingRecipe.java
+++ src/main/java/com/simomett/mycologymod/recipes/brewing/FungusBrewingRecipe.java
@@ -5,10 +5,10 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.alchemy.Potions;
 import net.neoforged.neoforge.common.brewing.IBrewingRecipe;
 
 import static com.simomett.mycologymod.datacomponents.ModDataComponentTypes.FUNGUS_GENOMA;
@@ -26,13 +26,7 @@ public class FungusBrewingRecipe implements IBrewingRecipe
 
     public boolean isInput(ItemStack stack)
     {
-        Item item = stack.getItem();
-        if(item == Items.POTION || item == Items.SPLASH_POTION || item == Items.LINGERING_POTION || item == Items.GLASS_BOTTLE)
-        {
-            ResourceLocation resourceLocation = ResourceLocation.withDefaultNamespace(stack.getComponents().get(DataComponents.POTION_CONTENTS).toString());
-            return resourceLocation.getPath().equals("awkward");
-        }
-        return false;
+        return stack.has(DataComponents.POTION_CONTENTS) && stack.get(DataComponents.POTION_CONTENTS).is(Potions.AWKWARD);
     }
 
     public boolean isIngredient(ItemStack ingredient)
