4c18dbc02ebd6a5dc7647c788f26e3c0c7a5f83b
matteo <matt.simonetti@hotmail.it>
2024-12-17 14:42:12 +0100

    Removed comments and useless stuff

--- src/main/java/com/simomett/mycologymod/MycologyMod.java
+++ src/main/java/com/simomett/mycologymod/MycologyMod.java
@@ -25,19 +25,13 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(MycologyMod.MODID)
-public class MycologyMod {
-    // Define mod id in a common place for everything to reference
+public class MycologyMod
+{
     public static final String MODID = "mycologymod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public MycologyMod(IEventBus evtBus, ModContainer modContainer)
     {
         modContainer.registerConfig(ModConfig.Type.COMMON, ModCommonConfigs.SPEC, MODID+"-common.toml");
