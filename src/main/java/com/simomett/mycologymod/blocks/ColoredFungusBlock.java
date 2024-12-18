7992aaf1aa1d1d7840a7d58126281ab1562c91d3
matteo <matt.simonetti@hotmail.it>
2024-12-18 21:49:41 +0100

    Fixed value 'is_lit' not initialized

--- src/main/java/com/simomett/mycologymod/blocks/ColoredFungusBlock.java
+++ src/main/java/com/simomett/mycologymod/blocks/ColoredFungusBlock.java
@@ -14,6 +14,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -63,6 +64,12 @@ public class ColoredFungusBlock extends BushBlock implements EntityBlock
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
         builder.add(IS_LIT);
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        return super.getStateForPlacement(context).setValue(IS_LIT, false);
     }
 
     @Override
