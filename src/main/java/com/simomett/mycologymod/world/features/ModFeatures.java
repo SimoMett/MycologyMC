af8a04ddc663933780de22e0f54f9abb547b2b1b
matteo <matt.simonetti@hotmail.it>
2024-12-18 19:29:47 +0100

    Generating 'Chromium ore' in the overworld

--- src/main/java/com/simomett/mycologymod/world/features/ModFeatures.java
+++ src/main/java/com/simomett/mycologymod/world/features/ModFeatures.java
@@ -3,6 +3,8 @@ package com.simomett.mycologymod.world.features;
 import com.simomett.mycologymod.MycologyMod;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.OreFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -12,4 +14,6 @@ public class ModFeatures
 
     public static final DeferredHolder<Feature<?>, SurfaceFungusFeatureConfiguration> SURFACE_FUNGUS = FEATURES.register("simple_fungus", SurfaceFungusFeatureConfiguration::new);
     public static final DeferredHolder<Feature<?>, UndergroundFungusFeatureConfiguration> UNDERGROUND_FUNGUS = FEATURES.register("underground_fungus", UndergroundFungusFeatureConfiguration::new);
+
+    public static final DeferredHolder<Feature<?>, OreFeature> ORE_CHROMIUM_SMALL = FEATURES.register("ore_chromium_small", () -> new OreFeature(OreConfiguration.CODEC));
 }
