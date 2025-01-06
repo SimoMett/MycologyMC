fa9ed8830460d563c4ea5c1b8de39672e1a6ac3c
matteo <matt.simonetti@hotmail.it>
2025-01-06 11:19:53 +0100

    Rendering fungus icon in WTHIT

--- src/main/java/com/simomett/mycologymod/compatibility/wthit/FungusBlockOverride.java
+++ src/main/java/com/simomett/mycologymod/compatibility/wthit/FungusBlockOverride.java
@@ -1,14 +1,28 @@
 package com.simomett.mycologymod.compatibility.wthit;
 
+import com.simomett.mycologymod.entities.ColoredFungusBlockEntity;
+import com.simomett.mycologymod.genetics.FungusGenoma;
 import mcp.mobius.waila.api.*;
+import mcp.mobius.waila.api.component.ItemComponent;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import static com.simomett.mycologymod.datacomponents.ModDataComponentTypes.FUNGUS_GENOMA;
 
 public class FungusBlockOverride implements IBlockComponentProvider
 {
     @Override
     public @Nullable ITooltipComponent getIcon(IBlockAccessor accessor, IPluginConfig config)
     {
-        //TODO fungus render icon
+        if(accessor.getBlockEntity() instanceof ColoredFungusBlockEntity fungusEntity)
+        {
+            //TODO optimization: I don't need to apply full genoma, just the species name
+            FungusGenoma fungusGenoma = fungusEntity.getFungusGenoma();
+            ItemStack itemStack = new ItemStack(accessor.getBlock().asItem());
+            itemStack.applyComponents(DataComponentMap.builder().set(FUNGUS_GENOMA, fungusGenoma).build());
+            return new ItemComponent(itemStack);
+        }
         return IBlockComponentProvider.super.getIcon(accessor, config);
     }
 
