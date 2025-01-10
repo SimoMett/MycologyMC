cf5a3a7ae4f78e7043fa745b0ac3397fd5f22294
matteo <matt.simonetti@hotmail.it>
2025-01-10 21:28:21 +0100

    Correctly syncing the colors map

--- src/main/java/com/simomett/mycologymod/data/FungusSpeciesColorsMap.java
+++ src/main/java/com/simomett/mycologymod/data/FungusSpeciesColorsMap.java
@@ -1,16 +1,45 @@
 package com.simomett.mycologymod.data;
 
+import com.simomett.mycologymod.MycologyMod;
 import com.simomett.mycologymod.genetics.FungusTraits;
+import com.simomett.mycologymod.network.serializable.IModSerializable;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.OnDatapackSyncEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
 import java.util.HashMap;
 
-public class FungusSpeciesColorsMap
+@EventBusSubscriber(modid = MycologyMod.MODID, bus = EventBusSubscriber.Bus.GAME)
+public class FungusSpeciesColorsMap implements CustomPacketPayload, IModSerializable
 {
+    public static final CustomPacketPayload.Type<FungusSpeciesColorsMap> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(MycologyMod.MODID, "fungus_colors_sync"));
     private final HashMap<String, int[]> colorsMap = new HashMap<>();
 
-    public static final FungusSpeciesColorsMap INSTANCE = new FungusSpeciesColorsMap();
+    public static FungusSpeciesColorsMap INSTANCE = new FungusSpeciesColorsMap();
 
     private FungusSpeciesColorsMap(){}
+
+    public static FungusSpeciesColorsMap fromByteBuf(FriendlyByteBuf byteBuf)
+    {
+        try
+        {
+            byte [] dst = new byte[byteBuf.readInt()];
+            byteBuf.readBytes(dst);
+            ByteArrayInputStream i = new ByteArrayInputStream(dst);
+            ObjectInputStream inputStream = new ObjectInputStream(i);
+            return INSTANCE = (FungusSpeciesColorsMap) inputStream.readObject();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 
     public final void put(FungusTraits defaultTraits, int[] colors)
     {
@@ -27,5 +56,20 @@ public class FungusSpeciesColorsMap
     public int[] get(String speciesName)
     {
         return colorsMap.getOrDefault(speciesName, new int[]{0, 0, 0, 0});
+    }
+
+    @Override
+    public Type<? extends CustomPacketPayload> type()
+    {
+        return TYPE;
+    }
+
+    @SubscribeEvent
+    public static void syncEvent(OnDatapackSyncEvent evt)
+    {
+        if(null != evt.getPlayer())
+            PacketDistributor.sendToPlayer(evt.getPlayer(), FungusSpeciesColorsMap.INSTANCE);
+        else
+            PacketDistributor.sendToAllPlayers(FungusSpeciesColorsMap.INSTANCE);
     }
 }
