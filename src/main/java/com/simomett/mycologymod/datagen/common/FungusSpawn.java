9979c5fa476367cdd5c09fc30850037e08cac7c6
matteo <matt.simonetti@hotmail.it>
2025-01-11 16:07:58 +0100

    Build 10: fixed mushrooms spawning in grid pattern and increased chance of finding the Lactarius proleferens

--- src/main/java/com/simomett/mycologymod/datagen/common/FungusSpawn.java
+++ src/main/java/com/simomett/mycologymod/datagen/common/FungusSpawn.java
@@ -1,33 +1,35 @@
 package com.simomett.mycologymod.datagen.common;
 
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 
 public class FungusSpawn
 {
+    private static final float DEFAULT_SPAWN_CHANCE = .4f;
     public static final FungusSpawn NO_SPAWN = null;
-    public static final FungusSpawn ANY_BIOME = new FungusSpawn("#any",.5f);
-    public static final FungusSpawn PLAINS = new FungusSpawn("minecraft:plains", .5f);
-    public static final FungusSpawn SUNFLOWER_PLAINS = new FungusSpawn(Biomes.SUNFLOWER_PLAINS, .5f);
-    public static final FungusSpawn FOREST = new FungusSpawn("minecraft:forest", .5f);
-    public static final FungusSpawn FLOWER_FOREST = new FungusSpawn(Biomes.FLOWER_FOREST, .5f);
-    public static final FungusSpawn DARK_FOREST = new FungusSpawn(Biomes.DARK_FOREST, .5f);
-    public static final FungusSpawn BIRCH_FOREST = new FungusSpawn("minecraft:birch_forest", .5f);
-    public static final FungusSpawn SNOWY_TAIGA = new FungusSpawn(Biomes.SNOWY_TAIGA, .5f);
-    public static final FungusSpawn OLD_GROWTH_BIRCH_FOREST = new FungusSpawn("minecraft:old_growth_birch_forest", .5f);
-    public static final FungusSpawn OLD_GROWTH_SPRUCE_TAIGA = new FungusSpawn("minecraft:old_growth_spruce_taiga", .5f);
-    public static final FungusSpawn OLD_GROWTH_PINE_TAIGA = new FungusSpawn("minecraft:old_growth_pine_taiga", .5f);
-    public static final FungusSpawn MEADOW = new FungusSpawn("minecraft:meadow", .5f);
-    public static final FungusSpawn SWAMP = new FungusSpawn(Biomes.SWAMP, .5f);
-    public static final FungusSpawn JUNGLE = new FungusSpawn(Biomes.JUNGLE, .5f);
-    public static final FungusSpawn MUSHROOM_FIELDS = new FungusSpawn(Biomes.MUSHROOM_FIELDS, .5f);
-    public static final FungusSpawn CAVES = new FungusSpawn("#caves", .5f);
-    public static final FungusSpawn NETHER_WASTES = new FungusSpawn(Biomes.NETHER_WASTES, .5f);
-    public static final FungusSpawn SOUL_SAND_VALLEY = new FungusSpawn(Biomes.SOUL_SAND_VALLEY, .5f);
-    public static final FungusSpawn END = new FungusSpawn(Biomes.THE_END, .5f);
-    public static final FungusSpawn END_MIDLANDS = new FungusSpawn(Biomes.END_HIGHLANDS, .5f);
-    public static final FungusSpawn END_HIGHLANDS = new FungusSpawn(Biomes.END_HIGHLANDS, .5f);
+    public static final FungusSpawn ANY_BIOME = new FungusSpawn("#any", DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn PLAINS = new FungusSpawn(Biomes.PLAINS, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn SUNFLOWER_PLAINS = new FungusSpawn(Biomes.SUNFLOWER_PLAINS, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn FOREST = new FungusSpawn(Biomes.FOREST, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn FLOWER_FOREST = new FungusSpawn(Biomes.FLOWER_FOREST, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn DARK_FOREST = new FungusSpawn(Biomes.DARK_FOREST, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn BIRCH_FOREST = new FungusSpawn(Biomes.BIRCH_FOREST, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn SNOWY_TAIGA = new FungusSpawn(Biomes.SNOWY_TAIGA, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn OLD_GROWTH_BIRCH_FOREST = new FungusSpawn(Biomes.OLD_GROWTH_BIRCH_FOREST, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn OLD_GROWTH_SPRUCE_TAIGA = new FungusSpawn(Biomes.OLD_GROWTH_SPRUCE_TAIGA, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn OLD_GROWTH_PINE_TAIGA = new FungusSpawn(Biomes.OLD_GROWTH_PINE_TAIGA, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn MEADOW = new FungusSpawn(Biomes.MEADOW, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn SWAMP = new FungusSpawn(Biomes.SWAMP, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn JUNGLE = new FungusSpawn(Biomes.JUNGLE, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn MUSHROOM_FIELDS = new FungusSpawn(Biomes.MUSHROOM_FIELDS, 1f);
+    public static final FungusSpawn CAVES = new FungusSpawn("#caves", DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn NETHER_WASTES = new FungusSpawn(Biomes.NETHER_WASTES, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn SOUL_SAND_VALLEY = new FungusSpawn(Biomes.SOUL_SAND_VALLEY, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn END = new FungusSpawn(Biomes.THE_END, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn END_MIDLANDS = new FungusSpawn(Biomes.END_HIGHLANDS, DEFAULT_SPAWN_CHANCE);
+    public static final FungusSpawn END_HIGHLANDS = new FungusSpawn(Biomes.END_HIGHLANDS, DEFAULT_SPAWN_CHANCE);
     public static final FungusSpawn VERY_RARE = new FungusSpawn("#any",.1f);//FIXME to adjust
 
     private String biomes;
