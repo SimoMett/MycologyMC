03dd031a91a1605af8c6ef6b1dfaa311bb6e1fa6
matteo <matt.simonetti@hotmail.it>
2025-01-27 19:32:26 +0100

    Changed recipes

--- src/main/java/com/simomett/mycologymod/datagen/recipe/crafting/FungusShapelessRecipesProvider.java
+++ src/main/java/com/simomett/mycologymod/datagen/recipe/crafting/FungusShapelessRecipesProvider.java
@@ -24,7 +24,26 @@ public class FungusShapelessRecipesProvider extends RecipeProvider
     @Override
     protected void buildRecipes()
     {
-        addCraftingRecipe(SLIME_FUNGUS, Items.SLIME_BALL, 2);
+        // Colors
+        addCraftingRecipe(YELLOW_FUNGUS, Items.YELLOW_DYE, 2);
+        addCraftingRecipe(ORANGE_FUNGUS, Items.ORANGE_DYE, 2);
+        addCraftingRecipe(AMANITA_RUBRA, Items.RED_DYE, 2);
+        addCraftingRecipe(OVULUS_VIOLACEUS, Items.PURPLE_DYE, 2);
+        addCraftingRecipe(CYAN_FUNGUS, Items.CYAN_DYE, 2);
+        addCraftingRecipe(BLUE_FUNGUS, Items.BLUE_DYE, 2);
+        addCraftingRecipe(LACTARIUS_VIRENS, Items.GREEN_DYE, 2);
+        addCraftingRecipe(WHITE_FUNGUS, Items.WHITE_DYE, 2);
+        addCraftingRecipe(GREY_FUNGUS, Items.GRAY_DYE, 2);
+        addCraftingRecipe(LIME_FUNGUS, Items.LIME_DYE, 2);
+        addCraftingRecipe(LIGHTGREY_FUNGUS, Items.LIGHT_GRAY_DYE, 2);
+        addCraftingRecipe(BLACK_FUNGUS, Items.BLACK_DYE, 2);
+        addCraftingRecipe(LIGHTBLUE_FUNGUS, Items.LIGHT_BLUE_DYE, 2);
+        addCraftingRecipe(PINK_FUNGUS, Items.PINK_DYE, 2);
+        addCraftingRecipe(MAGENTA_FUNGUS, Items.MAGENTA_DYE, 2);
+        addCraftingRecipe(BROWN_FUNGUS, Items.BROWN_DYE, 2);
+
+        // Materials
+        addCraftingRecipe(SLIME_FUNGUS, Items.SLIME_BALL);
         addCraftingRecipe(XEROCOMUS_FERRUGINEUS, Items.BONE_MEAL);
     }
 
