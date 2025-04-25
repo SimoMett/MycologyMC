af4f96d4e4959199b608ee5b8a14a3bebe70b802
matteo <matt.simonetti@hotmail.it>
2025-04-25 17:57:06 +0200

    Fixed returning item

--- src/main/java/com/simomett/mycologymod/blocks/FungusPotBlock.java
+++ src/main/java/com/simomett/mycologymod/blocks/FungusPotBlock.java
@@ -6,13 +6,13 @@ import com.simomett.mycologymod.entities.ColoredFungusBlockEntity;
 import com.simomett.mycologymod.genetics.FungusGenoma;
 import com.simomett.mycologymod.mixins.IFlowerPotBlockMixin;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
@@ -20,6 +20,8 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
@@ -51,6 +53,9 @@ public class FungusPotBlock extends FlowerPotBlock implements EntityBlock
     @Override
     protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult)
     {
+        if(stack.is(Items.AIR))
+            return InteractionResult.TRY_WITH_EMPTY_HAND;
+
         if(stack.has(ModDataComponentTypes.FUNGUS_GENOMA))
         {
             InteractionResult result = super.useItemOn(stack, state, level, pos, player, hand, hitResult);
