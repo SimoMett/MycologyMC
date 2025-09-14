45d594558e1b5e968d389434db46290347b7897f
SimoMett <matt.simonetti@hotmail.it>
2025-09-14 11:55:08 +0200

    [Fabric] Registered BlockEntity

--- fabric/src/main/java/com/simomett/mycologymod/MycologyMod.java
+++ fabric/src/main/java/com/simomett/mycologymod/MycologyMod.java
@@ -5,6 +5,7 @@ import com.simomett.mycologymod.creativetab.ModItemGroup;
 import com.simomett.mycologymod.data.FabricFungusSpeciesLoader;
 import com.simomett.mycologymod.data.FungusSpeciesSync;
 import com.simomett.mycologymod.datacomponents.DataComponentTypes;
+import com.simomett.mycologymod.entities.BlockEntitiesDefinitions;
 import com.simomett.mycologymod.items.ItemsDefinitions;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -28,6 +29,7 @@ public class MycologyMod implements ModInitializer {
         ModItemGroup.initialize();
         BlocksDefinitions.init();
         ItemsDefinitions.init();
+        BlockEntitiesDefinitions.init();
         ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new FabricFungusSpeciesLoader());
     }
 }
