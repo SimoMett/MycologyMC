435a43229e10cc2737057b5bcf8e6bee0eff4662
matteo <matt.simonetti@hotmail.it>
2025-09-23 21:57:24 +0200

    Testing new 'Last chance' mixin

--- common/src/main/java/com/simomett/mycologymod/effects/player/LastChanceEffect.java
+++ common/src/main/java/com/simomett/mycologymod/effects/player/LastChanceEffect.java
@@ -2,11 +2,13 @@ package com.simomett.mycologymod.effects.player;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.item.component.DeathProtection;
 
 import java.awt.*;
 
 public class LastChanceEffect extends MobEffect
 {
+    public static DeathProtection LAST_CHANCE = new DeathProtection(DeathProtection.TOTEM_OF_UNDYING.deathEffects());
     public static LastChanceEffect instance()
     {
         return new LastChanceEffect();
