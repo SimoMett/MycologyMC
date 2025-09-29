c91a62a22e56d248eb120ff584171fb7a8697012
matteo <matt.simonetti@hotmail.it>
2025-09-29 18:58:05 +0200

    Using CommonClass initializer

--- fabric/src/main/java/com/simomett/mycologymod/MycologyMod.java
+++ fabric/src/main/java/com/simomett/mycologymod/MycologyMod.java
@@ -1,41 +1,24 @@
 package com.simomett.mycologymod;
 
-import com.simomett.mycologymod.blocks.BlocksDefinitions;
 import com.simomett.mycologymod.creativetab.ModItemGroup;
 import com.simomett.mycologymod.data.FabricFungusSpeciesLoader;
 import com.simomett.mycologymod.data.FungusSpeciesSync;
-import com.simomett.mycologymod.datacomponents.DataComponentTypes;
-import com.simomett.mycologymod.effects.player.EffectsDefinitions;
-import com.simomett.mycologymod.entities.BlockEntitiesDefinitions;
-import com.simomett.mycologymod.items.ItemsDefinitions;
-import com.simomett.mycologymod.items.potions.Potions;
-import com.simomett.mycologymod.world.FeaturesDefinitions;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.PackType;
 
-public class MycologyMod implements ModInitializer {
-
+public class MycologyMod implements ModInitializer
+{
     @Override
     public void onInitialize()
     {
-
         // This method is invoked by the Fabric mod loader when it is ready
         // to load your mod. You can access Fabric and Common code in this
         // project.
 
-        // Use Fabric to bootstrap the Common mod.
-        Constants.LOG.info("Hello Fabric world!");
-        CommonClass.init();
-        EffectsDefinitions.init();
-        Potions.init();
-        DataComponentTypes.init();
         FungusSpeciesSync.registerPayloads();
         ModItemGroup.initialize();
-        BlocksDefinitions.init();
-        ItemsDefinitions.init();
-        BlockEntitiesDefinitions.init();
-        FeaturesDefinitions.init();
+        CommonClass.init();
         ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new FabricFungusSpeciesLoader());
     }
 }
--- neoforge/src/main/java/com/simomett/mycologymod/MycologyMod.java
+++ neoforge/src/main/java/com/simomett/mycologymod/MycologyMod.java
@@ -1,24 +1,17 @@
 package com.simomett.mycologymod;
 
-import com.simomett.mycologymod.blocks.BlocksDefinitions;
 import com.simomett.mycologymod.config.NeoForgeClientConfigs;
 import com.simomett.mycologymod.config.NeoForgeCommonConfigs;
 import com.simomett.mycologymod.creativetab.ModCreativeTabs;
 import com.simomett.mycologymod.data.FungusSpeciesLoader;
-import com.simomett.mycologymod.datacomponents.DataComponentTypes;
 import com.simomett.mycologymod.datacomponents.NeoForgeDataComponents;
-import com.simomett.mycologymod.effects.player.EffectsDefinitions;
 import com.simomett.mycologymod.effects.player.NeoForgeModEffects;
-import com.simomett.mycologymod.entities.BlockEntitiesDefinitions;
 import com.simomett.mycologymod.entities.NeoForgeBlockEntities;
-import com.simomett.mycologymod.items.ItemsDefinitions;
 import com.simomett.mycologymod.items.ModItems;
 import com.simomett.mycologymod.items.potions.NeoForgePotions;
-import com.simomett.mycologymod.items.potions.Potions;
 import com.simomett.mycologymod.particles.ModParticles;
 import com.simomett.mycologymod.recipes.breeding.MutationRecipeLoader;
 import com.simomett.mycologymod.recipes.brewing.FungusBrewingRecipeLoader;
-import com.simomett.mycologymod.world.FeaturesDefinitions;
 import com.simomett.mycologymod.world.features.NeoForgeFeatures;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -39,33 +32,28 @@ public class MycologyMod
         modContainer.registerConfig(ModConfig.Type.COMMON, NeoForgeCommonConfigs.INSTANCE.SPEC);
         modContainer.registerConfig(ModConfig.Type.CLIENT, NeoForgeClientConfigs.INSTANCE.SPEC);
 
-        DataComponentTypes.init();
+        CommonClass.init();
+
         NeoForgeDataComponents.DATA_COMPONENTS.register(evtBus);
 
-        ItemsDefinitions.init();
         ModItems.ITEMS.register(evtBus);
 
-        BlocksDefinitions.init();
         BLOCKS.register(evtBus);
 
         ModCreativeTabs.CREATIVE_TABS.register(evtBus);
 
-        BlockEntitiesDefinitions.init();
         NeoForgeBlockEntities.ENTITIES.register(evtBus);
 
         //DataComponentTypes.ATTACHMENT_TYPES.register(evtBus);
         ModParticles.PARTICLES.register(evtBus);
         //ModRecipes.RECIPE_SERIALIZERS.register(evtBus);
 
-        EffectsDefinitions.init();
         NeoForgeModEffects.EFFECTS.register(evtBus);
 
-        Potions.init();
         NeoForgePotions.POTIONS.register(evtBus);
 
         //ModMenus.MENU_TYPES.register(evtBus);
 
-        FeaturesDefinitions.init();
         NeoForgeFeatures.FEATURES.register(evtBus);
 
         NeoForge.EVENT_BUS.register(this);
