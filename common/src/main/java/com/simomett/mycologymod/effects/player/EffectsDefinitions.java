f6e6eb5ee083ece8206c549a6f218d183c2363ee
matteo <matt.simonetti@hotmail.it>
2026-06-02 17:43:24 +0200

    Added ReturnHomeEffect

--- common/src/main/java/com/simomett/mycologymod/effects/player/EffectsDefinitions.java
+++ common/src/main/java/com/simomett/mycologymod/effects/player/EffectsDefinitions.java
@@ -13,6 +13,7 @@ public class EffectsDefinitions
     public static final IRegisteredMobEffect<SensingEffect> SENSING = Services.PLATFORM.registerMobEffect("sensing", SensingEffect::create);
     public static final IRegisteredMobEffect<XPMultiplierEffect> XP_MULTIPLIER = Services.PLATFORM.registerMobEffect("xp_multiplier", XPMultiplierEffect::create);
     public static final IRegisteredMobEffect<TeleportingEffect> TELEPORTING = Services.PLATFORM.registerMobEffect("teleporting", TeleportingEffect::instance);
+    public static final IRegisteredMobEffect<ReturnHomeEffect> RETURN_HOME = Services.PLATFORM.registerMobEffect("return_home", ReturnHomeEffect::getInstance);
     public static final IRegisteredMobEffect<SampleEffect> SAMPLE = Services.PLATFORM.registerMobEffect("sample", SampleEffect::create);
 
     public static void init(){}
