28602d31786528891932d94baf8173aa3851a98e
matteo <matt.simonetti@hotmail.it>
2026-08-25 19:02:55 +0200

    Fixed crash

--- src/main/java/com/simomett/mycologymod/menu/AnalysingStationMenu.java
+++ src/main/java/com/simomett/mycologymod/menu/AnalysingStationMenu.java
@@ -1,11 +1,11 @@
 package com.simomett.mycologymod.menu;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.jspecify.annotations.Nullable;
@@ -25,22 +25,18 @@ public class AnalysingStationMenu extends AbstractContainerMenu
 
         this.container = container;
 
-        // Some containers do custom logic when opened by a player.
         container.startOpen(inventory.player);
 
-        // Add the slots for our container in a 3x3 grid.
-        //this.add3x3GridSlots();
-        this.addSlot(new Slot(this.container, 0, 26, 46));
-        this.addSlot(new Slot(this.container, 1, 75, 46));
-        this.addSlot(new Slot(this.container, 2, 133, 46));
+        this.addSlot(new Slot(this.container, 0, 27, 47));
+        this.addSlot(new Slot(this.container, 1, 76, 47));
+        this.addSlot(new Slot(this.container, 2, 134, 47));
 
-        // Add the player inventory slots.
         this.addStandardInventorySlots(inventory, INVENTORY_START_X, INVENTORY_START_Y);
     }
 
     public AnalysingStationMenu(final int containerId, final Inventory inventory)
     {
-        this(containerId, inventory, new SimpleContainer(1));
+        this(containerId, inventory, new SimpleContainer(3));
     }
 
     @Override
@@ -59,6 +55,12 @@ public class AnalysingStationMenu extends AbstractContainerMenu
     public void removed(Player player)
     {
         super.removed(player);
+        if(player instanceof ServerPlayer serverPlayer)
+        {
+            serverPlayer.addItem(this.container.getItem(0));
+            serverPlayer.addItem(this.container.getItem(1));
+            serverPlayer.addItem(this.container.getItem(2));
+        }
         this.container.stopOpen(player);
     }
 }
